c1ebd2aa341718308a458aae643e5ef6567c698f
narendrasoni1989 <narendrasoni2@gmail.com>
2016-01-21 17:36:55 +0800

    path updated to relative

--- config/env/development.js
+++ config/env/development.js
@@ -1,6 +1,6 @@
 module.exports = {
     db: 'mongodb://127.0.0.1/chatappdb_dev',
     privateKey: ' private -key value',
-    tmp:'/Users/narendra/Documents/workspace/NodeJsWorkspace/ChatApp/tmp',
-    upload:'/Users/narendra/Documents/workspace/NodeJsWorkspace/ChatApp/uploads'
+    tmp:'./tmp',
+    upload:'./uploads'
 };
